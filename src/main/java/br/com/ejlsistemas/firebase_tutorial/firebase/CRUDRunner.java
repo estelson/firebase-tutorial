ac1c6408e0e0c637d346f66eba11a35ada21a3a5
Estelson Medeiros Pereira <estelson@gmail.com>
2022-11-22 17:09:15 -0300

    Configuração da inicialização do Spring Boot  e autorização do Firebase

--- src/main/java/br/com/ejlsistemas/firebase_tutorial/firebase/CRUDRunner.java
+++ src/main/java/br/com/ejlsistemas/firebase_tutorial/firebase/CRUDRunner.java
@@ -1,16 +1,40 @@
+package br.com.ejlsistemas.firebase_tutorial.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author estelson
  */
+@SpringBootApplication
 public class CRUDRunner {
-    
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        ClassLoader classLoader = CRUDRunner.class.getClassLoader();
+
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        SpringApplication.run(CRUDRunner.class, args);
     }
-    
+
 }
